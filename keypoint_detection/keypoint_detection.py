3717c60c8c99a8b0f42b1518420b092c961c8639
maxthibeau <thibeaumax@gmail.com>
2019-02-08 11:37:13 -0700

    corner detection but points are filtered

--- keypoint_detection/keypoint_detection.py
+++ keypoint_detection/keypoint_detection.py
@@ -1,8 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from convoluution import *
-I = plt.imread('chessboard.png')
+from scipy import signal
 
+def pull_local_maxima(I):
+  local_maxima = []
+  for i in range(1, I.shape[0]-1):
+    for j in range(1, I.shape[1]-1):
+      if (I[i+1][j] <= I[i][j] 
+      and I[i-1][j] <= I[i][j] 
+      and I[i][j+1] <= I[i][j] 
+      and I[i+1][j-1] <= I[i][j]):
+        local_maxima.append((i, j, I[i,j]))
+
+  local_maxima.sort(key=lambda local_maxima: local_maxima[2], reverse = True) 
+  local_maxima = np.array(local_maxima)
+  local_maxima = local_maxima[:local_maxima.shape[0]/20]
+  radii = np.zeros((local_maxima.shape[0],1))
+  local_maxima = np.hstack((local_maxima, radii))
+
+  for i in range(0, local_maxima.shape[0]):
+    r = float("inf")
+    for j in range(0, local_maxima.shape[0]):
+      distance = np.sqrt((local_maxima[i][0] - local_maxima[j][0])**2 + (local_maxima[i][1] - local_maxima[j][1])**2)
+    if (local_maxima[j][2] > local_maxima[i][2]):
+      r = distance 
+    local_maxima[i][3] = r
+
+  indices = np.argsort(-local_maxima[:,3])
+  local_maxima = local_maxima[indices]
+  return local_maxima[:100]
+
+I = plt.imread('statue_of_liberty.jpg')
+I = I.mean(axis=2)
 Su = np.matrix(
 [[-1, 0, 1],
 [-2, 0, 2],
@@ -16,14 +45,17 @@ w = np.matrix([
 [0.023528,	0.033969,	0.038393,	0.033969,	0.023528]
 ])
 
-Iu = convolve_no_edge(I, Su)
-Iv = convolve_no_edge(I, Su.T)
+Iu = signal.convolve2d(I, Su)
+Iv = signal.convolve2d(I, Su.T)
 
-Iuu = convolve_no_edge(np.multiply(Iu, Iu), w)
-Ivv = convolve_no_edge(np.multiply(Iv, Iv), w)
-Iuv = convolve_no_edge(np.multiply(Iu, Iv), w)
+Iuu = signal.convolve2d(np.multiply(Iu, Iu), w)
+Ivv = signal.convolve2d(np.multiply(Iv, Iv), w)
+Iuv = signal.convolve2d(np.multiply(Iu, Iv), w)
 
 H = np.divide(np.multiply(Iuu, Ivv) - np.multiply(Iuv, Iuv), Iuu + Ivv + 1e-10)
 
-plt.imshow(H)
+local_maxima = pull_local_maxima(H)
+
+plt.imshow(I)
+plt.scatter(local_maxima[:,1], local_maxima[:,0], c=local_maxima[:,2])
 plt.show() 
